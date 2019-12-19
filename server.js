418b6047bf139717795257da5f5932b9973eb9a4
jamierachael <jamie.rachael@gmail.com>
2019-12-19 17:28:28 -0500

    Added all inquier questions, added switch statements

--- server.js
+++ server.js
@@ -74,6 +74,20 @@ function runSearch() {
                             manager_id: 10,
                         }
                     ]);
+                    inquirer
+                        .prompt(
+
+                            {
+                                name: "next",
+                                type: "list",
+                                message: "What would you like to do next?",
+                                choices: [
+                                    "Add employee",
+                                    "Remove employee",
+                                    "Update employee role",
+                                    "Update employee manager"
+                                ]
+                            })
                     break;
                 // Start new case
                 case "View all employees by department":
@@ -103,68 +117,221 @@ function runSearch() {
                             ]);
                         })
 
-                // break;
+                    break;
                 // Start new case
-                // case "View all employees by department":
-                //     inquirer
-                //     // .prompt({
-                //     //     name: "department",
-                //     //     type: "list",
-                //     //     message: "What would you like to do?",
-                //     //     choices: [
-                //     //         "View all employees",
-                //     //         "View all employees by department",
-                //     //         "View all employees by manager",
-                //     //         "Add employee",
-                //     //         "Remove employee",
-                //     //         "Update employee role",
-                //     //         "Update employee manager"
-                //     //     ]
-                //     // })
-                //     // }).then(answersId => {
-                //     // Run Query - filter by id - display table
-                //     // })
+                case "View all employees by manager":
+                    console.table([
+                        {
+                            table: 'employee',
+                            id: 'foo',
+                            first_name: 10,
+                            last_name: 10,
+                            role_id: 10,
+                            manager_id: 10,
+                        }
+                    ]);
 
-                // break;
-                // Start new case
-                // case "View all employees by department":
-                //     inquirer
-                //     // .prompt({
-                //     //     name: "department",
-                //     //     type: "list",
-                //     //     message: "What would you like to do?",
-                //     //     choices: [
-                //     //         "View all employees",
-                //     //         "View all employees by department",
-                //     //         "View all employees by manager",
-                //     //         "Add employee",
-                //     //         "Remove employee",
-                //     //         "Update employee role",
-                //     //         "Update employee manager"
-                //     //     ]
-                //     // })
-                //     // }).then(answersId => {
-                //     // Run Query - filter by id - display table
-                //     // })
+                    inquirer
+                        .prompt({
+                            name: "manager",
+                            type: "list",
+                            message: "Which employee's manager would you like to update?",
+                            choices: [
+                                "Person 1",
+                                "Person 2",
+                                "Person 3",
+                                "Person 4",
+                                "Person 5"
+                            ]
+                        }).then(answersSetManager => {
 
-                // break;
+                            inquirer
+                                .prompt({
+                                    name: "setmanager",
+                                    type: "list",
+                                    message: "Which employee do you want to set as manager for the selected employee?",
+                                    choices: [
+                                        "Person 1",
+                                        "Person 2",
+                                        "Person 3",
+                                        "Person 4",
+                                        "Person 5"
+                                    ]
+                                })
+                        })
+
+                    //     // Run Query - filter by id - display table
+                    //     // })
+
+                    break;
                 // Start new case
-                // case "View all employees by department":
-                //     inquirer
-                //     // .prompt({
-                //     //     name: "department",
-                //     //     type: "list",
-                //     //     message: "What would you like to do?",
-                //     //     choices: [
-                //     //         "View all employees",
-                //     //         "View all employees by department",
-                //     //         "View all employees by manager",
-                //     //         "Add employee",
-                //     //         "Remove employee",
-                //     //         "Update employee role",
-                //     //         "Update employee manager"
-                //     //     ]
-                //     // })
+                case "Add employee":
+                    inquirer
+                        .prompt([
+                            {
+                                name: "employeeFirst",
+                                type: "input",
+                                message: "What is the employee's first name?",
+                                validate: answer => {
+                                    if (answer !== "") {
+                                        return true;
+                                    }
+                                    return "Please enter at least one character.";
+                                }
+                            },
+                            // How do we prompt the next input question?
+                            {
+                                name: "employeeLast",
+                                type: "input",
+                                message: "What is the employee's last name?",
+                                validate: answer => {
+                                    if (answer !== "") {
+                                        return true;
+                                    }
+                                    return "Please enter at least one character.";
+                                }
+                            },
+                            {
+                                name: "department",
+                                type: "list",
+                                message: "What is the employee's role?",
+                                choices: [
+                                    "Sales",
+                                    "Finance",
+                                    "Engineering",
+                                    "Legal"
+                                ]
+                            },
+                            {
+                                name: "manager",
+                                type: "list",
+                                message: "who is the employee's manager?",
+                                choices: [
+                                    "Person 1",
+                                    "Person 2",
+                                    "Person 3",
+                                    "Person 4",
+                                    "Person 5"
+                                ]
+                            }
+                        ])
+                    break;
+
+                case "Remove employee":
+                    inquirer
+                        .prompt([
+                            {
+                                name: "employeeFirst",
+                                type: "input",
+                                message: "What is the employee's first name?",
+                                validate: answer => {
+                                    if (answer !== "") {
+                                        return true;
+                                    }
+                                    return "Please enter at least one character.";
+                                }
+                            },
+                            // How do we prompt the next input question?
+                            {
+                                name: "employeeLast",
+                                type: "input",
+                                message: "What is the employee's last name?",
+                                validate: answer => {
+                                    if (answer !== "") {
+                                        return true;
+                                    }
+                                    return "Please enter at least one character.";
+                                }
+                            },
+                            {
+                                name: "department",
+                                type: "list",
+                                message: "What is the employee's role?",
+                                choices: [
+                                    "Sales",
+                                    "Finance",
+                                    "Engineering",
+                                    "Legal"
+                                ]
+                            },
+                            {
+                                name: "manager",
+                                type: "list",
+                                message: "who is the employee's manager?",
+                                choices: [
+                                    "Person 1",
+                                    "Person 2",
+                                    "Person 3",
+                                    "Person 4",
+                                    "Person 5"
+                                ]
+                            }
+                        ])
+                    break;
+
+                // }).then(answersId => {
+                //     // Run Query - filter by id - display table
+
+                // Start new case
+                case "Update employee role":
+                    inquirer
+                        .prompt([
+                            {
+                                name: "updatemanager",
+                                type: "list",
+                                message: "Which employee's role do you want to update?",
+                                choices: [
+                                    "Person 1",
+                                    "Person 2",
+                                    "Person 3",
+                                    "Person 4",
+                                    "Person 5"
+                                ]
+                            },
+                            {
+                                name: "department",
+                                type: "list",
+                                message: "What is the employee's new role?",
+                                choices: [
+                                    "Sales",
+                                    "Finance",
+                                    "Engineering",
+                                    "Legal"
+                                ]
+                            }
+                        ])
+
+                    break;
+
+
+                case "Update employee manager":
+                    inquirer
+                        .prompt([
+                            {
+                                name: "updatemanager",
+                                type: "list",
+                                message: "Which employee do you want to update the manager for?",
+                                choices: [
+                                    "Person 1",
+                                    "Person 2",
+                                    "Person 3",
+                                    "Person 4",
+                                    "Person 5"
+                                ]
+                            },
+                            {
+                                name: "manager",
+                                type: "list",
+                                message: "who is the employee's manager?",
+                                choices: [
+                                    "Person 1",
+                                    "Person 2",
+                                    "Person 3",
+                                    "Person 4",
+                                    "Person 5"
+                                ]
+                            }
+                        ])
                 //     // }).then(answersId => {
                 //     // Run Query - filter by id - display table
                 //     // })
@@ -172,8 +339,6 @@ function runSearch() {
                 //         default:
                 // buildTeam();
             }
-
-
 
         });
 
